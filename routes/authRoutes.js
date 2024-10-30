43df7ece0be2528429e1be5a40f5a66b9e23bb87
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-10-30 10:15:32 +0530

    authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,26 +12,12 @@ const {
 } = require('../controllers/authController')
 
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 
-const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
 
-    if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user; // Assuming decoded.user contains the user object
-        next();
-    } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
-    }
-};
 router.post('/signup',upload.single("uploadImage"), signup);
 router.post('/login', login);
-router.get('/users',authMiddleware, getAllUsers); 
+router.get('/users', getAllUsers); 
 router.get('/users/:id', getUser); 
 router.put('/update/:id',upload.single("uploadImage"), updateUser); 
 router.delete('/users/:id', deleteUser); 
