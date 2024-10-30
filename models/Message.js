2e55622f399f93f3f2923ac679749c6c18da94c3
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-10-30 12:39:00 +0530

    Message.js

--- models/Message.js
+++ models/Message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
     roomId: { type: String, },
-    sender: { type:String },
+    sender: {  type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     text: { type: String, },
     timestamp: { type: Date, default: Date.now },
 });
