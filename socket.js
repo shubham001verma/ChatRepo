8ad4f5c445283a710630db730f36487e531777c8
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-10-30 07:49:03 +0530

    socket.js

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 const socketIO = require('socket.io');
-const Message = require('./models/Message');
 
+const cors = require('cors');
 const socketHandler = (server) => {
     const io = socketIO(server, {
         cors: {
@@ -9,21 +9,18 @@ const socketHandler = (server) => {
     });
 
     io.on('connection', (socket) => {
-        console.log(`User connected: ${socket.id}`);
-
         socket.on('joinRoom', (roomId) => {
             socket.join(roomId);
-            console.log(`User ${socket.id} joined room: ${roomId}`);
         });
-
-        socket.on('sendMessage', async (data) => {
-            const { roomId, sender, text } = data;
-            const message = new Message({ roomId, sender, text });
-            await message.save();
-
-            io.to(roomId).emit('message', message);
+    
+        socket.on('leaveRoom', (roomId) => {
+            socket.leave(roomId);
         });
-
+    
+        socket.on('sendMessage', (message) => {
+            io.to(message.roomId).emit('message', message);
+        });
+    
         socket.on('disconnect', () => {
             console.log('User disconnected');
         });
