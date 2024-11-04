17fcd70e02fc0085a3583b44a9adf60c70c69568
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-11-04 14:08:22 +0530

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { saveMessage, getMessages,clearMessages,deleteMessage } = require('../controllers/chatController');
+const { saveMessage, getMessages,clearMessages,deleteMessage ,updateMessage,notification} = require('../controllers/chatController');
 const upload = require('../middleware/Multer')
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.post('/messages',upload.single("image"), saveMessage);
 router.get('/messages/:roomId', getMessages);
 router.delete('/messages/clear/:roomId',clearMessages);
 router.delete('/message/delete/:roomId/:messageId',deleteMessage);
+router.get('/notification/:roomId',notification);
+router.put('/message/update/:roomId/:messageId',updateMessage);
 
 module.exports = router;
