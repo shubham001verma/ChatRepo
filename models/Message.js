2c51af8fecee25f68799cf2ed55685ecb100b4bb
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-11-05 21:19:25 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -4,9 +4,9 @@ const messageSchema = new mongoose.Schema({
     roomId: { type: String, },
     sender: {  type: String },
     text: { type: String, },
-      image:  [{ type: String }] ,
-    video: [{ type: String }] ,
-    pdf:  [{ type: String }] ,
+      image:  { type: [String] } ,
+    video: { type: [String ]} ,
+    pdf:  { type: [String ]} ,
       read: { type: Boolean, default: false },
     timestamp: { type: Date, default: Date.now },
 });
