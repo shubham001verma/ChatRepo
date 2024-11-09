830b895a5a3a706b7441afd5aa599dde73ec1923
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-11-09 08:58:01 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -156,21 +156,21 @@ exports.checkblock=async(req,res)=>{
         res.status(500).json({ message: 'An error occurred', error });
     }
 }
-// exports.isblock=async(req,res)=>{
-//      const { userId } = req.params;
+exports.isblock=async(req,res)=>{
+     const { userId } = req.params;
 
-//     try {
-//         const user = await User.findById(userId);
+    try {
+        const user = await User.findById(userId);
         
-//         if (!user) {
-//             return res.status(404).json({ message: 'User not found' });
-//         }
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
 
-//         res.status(200).json({ isBlocked: user.isBlocked });
-//     } catch (error) {
-//         res.status(500).json({ message: 'Server error', error });
-//     }
-// }
+        res.status(200).json({ isBlocked: user.isBlocked });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+}
 
 // Delete user
 exports.deleteUser = async (req, res) => {
