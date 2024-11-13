06279a321c2ba18526946e7d376b365c4960bfa4
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-11-13 23:11:30 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String,  },
-    email: { type: String,  unique: true },
+    email: { type: String,  },
     password: { type: String, },
     uploadImage: {
         type: String, // URL or path to the user's avatar image
