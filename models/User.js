dd60e38fcf9847507cd6990c9d09516fee5d1817
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-11-13 21:35:03 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -15,8 +15,8 @@ const userSchema = new mongoose.Schema({
         type: Number,
         required: true,
         unique: true,
-        min:10,
-        max:10,      
+        min: 1000000000, // minimum 10-digit number (e.g., 1000000000)
+    max: 9999999999, // maximum 10-digit number (e.g., 9999999999)      
     
     },
       DateofBirth:{
