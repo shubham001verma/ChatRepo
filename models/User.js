24f583d55a52a28cd1afb2d3ee9e6adfcb9c2e24
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-11-13 21:46:07 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -16,8 +16,14 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         min: 1000000000, // minimum 10-digit number (e.g., 1000000000)
-    max: 9999999999, // maximum 10-digit number (e.g., 9999999999)      
-    
+        max: 9999999999, // maximum 10-digit number (e.g., 9999999999)      
+        validate: {
+        validator: function(v) {
+            return Number.isInteger(v); // ensures it's an integer
+        },
+        message: props => `${props.value} is not a valid integer!`
+    }
+
     },
       DateofBirth:{
         type: String,
