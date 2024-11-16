72e2abd59bb692c8bf725e03c948ca28c50f0ca9
shubham001verma <147648483+shubham001verma@users.noreply.github.com>
2024-11-16 22:24:58 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String,  },
-    email: { type: String,  },
+    email: { type: String, unique:true },
     password: { type: String, },
     uploadImage: {
         type: String, // URL or path to the user's avatar image
@@ -12,17 +12,8 @@ const userSchema = new mongoose.Schema({
         default: Date.now,
       },
           mobile: {
-        type: Number,
-        required: true,
-        unique: true,
-        min: 1000000000, // minimum 10-digit number (e.g., 1000000000)
-        max: 9999999999, // maximum 10-digit number (e.g., 9999999999)      
-        validate: {
-        validator: function(v) {
-            return Number.isInteger(v); // ensures it's an integer
-        },
-        message: props => `${props.value} is not a valid integer!`
-    }
+        type: String,
+       
 
     },
       DateofBirth:{
